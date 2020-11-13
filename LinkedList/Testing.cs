8d1047dcdb3850e708112171610fbd78caee7e2c
ewilsandman <56822177+ewilsandman@users.noreply.github.com>
2020-11-13 13:24:22 +0100

    aea

---
+++ LinkedList/Testing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class TestMethod
+    {
+        [TestMethod]
+        public void TestAddition()
+        {
+            //Assign
+            MyLinkedList<int> list = new MyLinkedList<int>();
+            int value = 5;
+
+            //Act
+            var node = list.AddFirst(value);
+            list.AddFirst(2);
+            list.AddFirst(3);
+            list.AddFirst(4);
+
+            //Assert
+            Assert.AreEqual(value, node.Data);
+            Assert.AreEqual(4, list.Count);
+            Assert.AreEqual(true, list.Contains(3));
+        }
+    }
+}
